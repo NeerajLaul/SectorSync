9aed7f571ba28cb53d1fcebdb61ef523a809752b
NeerajLaul <135740179+NeerajLaul@users.noreply.github.com>
2025-10-22 14:34:48 +0530

    Update server.js
    Updated to work with Render

--- server.js
+++ server.js
@@ -45,4 +45,6 @@ app.post("/api/scoring", (req, res) => {
 // answers (mounted router)  ✅ single source of truth
 app.use("/api/answers", answersRouter);
 
-app.listen(5000, () => console.log("✅ Server running at http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
