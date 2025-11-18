de1758f34bf885be538a2766bbc00a3a1164ad4f
NeerajLaul <135740179+NeerajLaul@users.noreply.github.com>
2025-11-18 23:28:09 +0530

    Update results.tsx
    Made api calls dynamic for dev

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -159,7 +159,7 @@ export function ResultsPage({
   useEffect(() => {
     if (!results || !results.ranking?.length || results.restored) return;
 
-    fetch("http://localhost:5000/api/answers", {
+    fetch("/api/answers", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
